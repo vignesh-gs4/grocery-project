adc0f886d1a6e64c407b0a0a3862162d60e65a28
vignesh-gs4 <vignesh04gs@gmail.com>
2026-03-22 20:30:02 +0530

    cloudinary error fix

--- server/server.js
+++ server/server.js
@@ -5,7 +5,6 @@ import "dotenv/config"
 import connectDB from "./config/db.js";
 import userRouter from "./routes/userRoute.js";
 import sellerRouter from "./routes/sellerRoute.js";
-import connectCloudinary from "./config/cloudinary.js";
 import productRouter from "./routes/productRoute.js";
 import cartRouter from "./routes/cartRouter.js";
 import addressRouter from "./routes/addressRoute.js";
@@ -16,7 +15,6 @@ const app = express();
 const port = process.env.PORT | 4000;
 
 await connectDB();
-await connectCloudinary();
 const allowOrigins = ['http://localhost:5173', 'https://grocery-project-lyart.vercel.app'];
 
 app.post("/stripe", express.raw({ type: 'application/json' }), stripeWebhooks);
